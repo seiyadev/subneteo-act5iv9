b3760bee2153c6bd184fac917f86799c98516d88
Axel Chávez <107567624+seiyadev@users.noreply.github.com>
2022-09-13 02:22:47 -0500

    0.5

--- subnt.js
+++ subnt.js
@@ -37,9 +37,9 @@ function convertirDecimal(bin) {
 
 function convertirABinario(num) {
     let binario = (num % 2).toString();
-    for (; num > 1; ) {
+    for (; num > 1;) {
         num = parseInt(num / 2);
-        binario =  (num % 2) + (binario);
+        binario = (num % 2) + (binario);
     }
     return binario;
 }
@@ -54,159 +54,148 @@ function generarPorSubRed() {
     let mascaraSubred = new Array;
     let ms = "";
 
-    if (nslt <= 126) {
-        if (nbits == 8) {
-            mascaraSubred = ["11111111", "00000000", "00000000", "00000000"];
-        } else {
-            if (nbits == 16) {
-                mascaraSubred = ["11111111", "11111111", "00000000", "00000000"];
-            } else {
-                if (nbits == 24) {
-                    mascaraSubred = ["11111111", "11111111", "11111111", "00000000"];
-                }
-            }
-        }
-
-
-        for (var i = 1; i < 8; i++) { //Calcula la mascara de subred
-            if ((2 ** i - 2) >= nslt) {
-                for (var y = 0; y < i; y++) {
-                    ms += "1";
-                }
-                for (var z = 0; z < 8 - i; z++) {
-                    ms += "0";
-                }
-
-                for (var zx = 0; zx < mascaraSubred.length; zx++) {
-                    if (mascaraSubred[zx] == "00000000") {
-                        mascaraSubred[zx] = ms;
-                        break;
-                    }
-                }
-                console.log("Máscara de subred calculada: " + mascaraSubred);
-                break;
-            }
-        }
-
-        let mascaraSubredimp = "";
-        for (var i = 0; i < mascaraSubred.length; i++) {
-            if (i < mascaraSubred.length - 1) {
-                mascaraSubredimp = mascaraSubredimp + mascaraSubred[i] + ".";
-            } else {
-                mascaraSubredimp += mascaraSubred[i];
-            }
-        }
-
-        mascaraSubRedDecimal = convertirDecimal(mascaraSubred);
-        console.log(mascaraSubRedDecimal);
-        let numS;
-        if (mascaraSubRedDecimal[0] < 255) {
-            numS = mascaraSubRedDecimal[0];
-        } else {
-            if (mascaraSubRedDecimal[1] < 255) {
-                numS = mascaraSubRedDecimal[1];
-            } else {
-                if (mascaraSubRedDecimal[3] < 255) {
-                    numS = mascaraSubRedDecimal[3];
-                }
-            }
-        }
-        let saltosRed = 256 - numS;
-        let cc = 0;
-
-        for (let i = 0; i < mascaraSubred.length; i++) {
-            var ps = mascaraSubred[i];
-            for (let y = 0; y < ps.length; y++) {
-                if (ps[y] == 0) {
-                    cc += 1;
-                }
-            }
-        }
-        let hosts = 2 ** cc - 2;
-
-        let vp1 = convertirABinario(ip1);
-        let vp2 = convertirABinario(ip2);
-        let vp3 = convertirABinario(ip3);
-        let vp4 = convertirABinario(ip4);
-
-        let ipobt = new Array; //Ip subred obtenida
-        ipobt.push(vp1);
-        ipobt.push(vp2);
-        ipobt.push(vp3);
-        ipobt.push(vp4);
-        console.log(ipobt);
-
-
-
-
-
-
-
-
-        let direcciones = new Array(nslt);
-        let direcciones2 = new Array(nslt);
-        for (let i = 0; i < direcciones.length; i++) {
-            direcciones[i] = { numerosubred: i+1, subred: ipobt, desde: "nd", hasta: 1, broadcast: 1 };
-        }
-
-        let tablaDirecciones = document.getElementById("direcciones");
-        let cuerpoTabla = document.createElement("tbody");
-
-        direcciones.forEach(p => {
-            let fila = document.createElement("tr");
-
-            td = document.createElement("td");
-            td.innerText = p.numerosubred;
-            fila.appendChild(td);
-
-            td = document.createElement("td");
-            td.innerText = p.subred;
-            fila.appendChild(td);
-
-            td = document.createElement("td");
-            td.innerText = p.desde;
-            fila.appendChild(td);
-
-            td = document.createElement("td");
-            td.innerText = p.hasta;
-            fila.appendChild(td);
-
-            td = document.createElement("td");
-            td.innerText = p.hasta;
-            fila.appendChild(td);
-
-            cuerpoTabla.appendChild(fila);
-            
-        });
-        tablaDirecciones.appendChild(cuerpoTabla);
-
-
-        for (let i = 0; i < direcciones2.length; i++) {
-            direcciones2[i] = { SaltosSubRed: saltosRed, nhosts: hosts, mascara: mascaraSubred};
-        }
-
-        let tablaDirecciones2 = document.getElementById("direcciones2");
-        let cuerpoTabla2 = document.createElement("tbody");
-
-        direcciones2.forEach(p => {
-            let fila = document.createElement("tr");
-
-            td = document.createElement("td");
-            td.innerText = p.SaltosSubRed;
-            fila.appendChild(td);
-
-            td = document.createElement("td");
-            td.innerText = p.nhosts;
-            fila.appendChild(td);
-
-            td = document.createElement("td");
-            td.innerText = p.mascara;
-            fila.appendChild(td);
-
-            cuerpoTabla2.appendChild(fila);
-        });
-        tablaDirecciones2.appendChild(cuerpoTabla2);
+    if (nbits == 8) {
+        mascaraSubred = ["11111111", "00000000", "00000000", "00000000"];
     } else {
-        alert("Hubo un error con los datos ingresados.\nMáximo de subredes: 126");
+        if (nbits == 16) {
+            mascaraSubred = ["11111111", "11111111", "00000000", "00000000"];
+        } else {
+            if (nbits == 24) {
+                mascaraSubred = ["11111111", "11111111", "11111111", "00000000"];
+            }
+        }
     }
+
+
+    for (var i = 1; i < 8; i++) { //Calcula la mascara de subred
+        if ((2 ** i - 2) >= nslt) {
+            for (var y = 0; y < i; y++) {
+                ms += "1";
+            }
+            for (var z = 0; z < 8 - i; z++) {
+                ms += "0";
+            }
+
+            for (var zx = 0; zx < mascaraSubred.length; zx++) {
+                if (mascaraSubred[zx] == "00000000") {
+                    mascaraSubred[zx] = ms;
+                    break;
+                }
+            }
+            console.log("Máscara de subred calculada: " + mascaraSubred);
+            break;
+        }
+    }
+
+    let mascaraSubredimp = "";
+    for (var i = 0; i < mascaraSubred.length; i++) {
+        if (i < mascaraSubred.length - 1) {
+            mascaraSubredimp = mascaraSubredimp + mascaraSubred[i] + ".";
+        } else {
+            mascaraSubredimp += mascaraSubred[i];
+        }
+    }
+
+    mascaraSubRedDecimal = convertirDecimal(mascaraSubred);
+    console.log(mascaraSubRedDecimal);
+    let numS;
+    if (mascaraSubRedDecimal[0] < 255) {
+        numS = mascaraSubRedDecimal[0];
+    } else {
+        if (mascaraSubRedDecimal[1] < 255) {
+            numS = mascaraSubRedDecimal[1];
+        } else {
+            if (mascaraSubRedDecimal[3] < 255) {
+                numS = mascaraSubRedDecimal[3];
+            }
+        }
+    }
+    let saltosRed = 256 - numS;
+    let cc = 0;
+
+    for (let i = 0; i < mascaraSubred.length; i++) {
+        var ps = mascaraSubred[i];
+        for (let y = 0; y < ps.length; y++) {
+            if (ps[y] == 0) {
+                cc += 1;
+            }
+        }
+    }
+    let hosts = 2 ** cc - 2;
+
+    let ipdec = new Array; //Ip subred obtenida en decimal
+    ipdec.push(ip1);
+    ipdec.push(ip2);
+    ipdec.push(ip3);
+    ipdec.push(ip4);
+
+    let direcciones = new Array(nslt);
+    let direcciones2 = new Array(nslt);
+    for (let i = 0; i < direcciones.length; i++) {
+        ipdec[3] += saltosRed;
+        let bin1 = convertirABinario(ipdec[0]);
+        let bin2 = convertirABinario(ipdec[1]);
+        let bin3 = convertirABinario(ipdec[2]);
+        let bin4 = convertirABinario(ipdec[3]);
+        let arr = [ipdec[0], ipdec[1], ipdec[2], ipdec[3]];
+        let arr2 = [bin1, bin2, bin3, bin4];
+        direcciones[i] = { numerosubred: i + 1, subred: arr, desde: 2, hasta: 1, broadcast: 1 };
+    }
+    let tablaDirecciones = document.getElementById("direcciones");
+    let cuerpoTabla = document.createElement("tbody");
+
+    direcciones.forEach(p => {
+        let fila = document.createElement("tr");
+
+        td = document.createElement("td");
+        td.innerText = p.numerosubred;
+        fila.appendChild(td);
+
+        td = document.createElement("td");
+        td.innerText = p.subred;
+        fila.appendChild(td);
+
+        td = document.createElement("td");
+        td.innerText = p.desde;
+        fila.appendChild(td);
+
+        td = document.createElement("td");
+        td.innerText = p.hasta;
+        fila.appendChild(td);
+
+        td = document.createElement("td");
+        td.innerText = p.hasta;
+        fila.appendChild(td);
+
+        cuerpoTabla.appendChild(fila);
+
+    });
+    tablaDirecciones.appendChild(cuerpoTabla);
+
+
+    for (let i = 0; i < direcciones2.length; i++) {
+        direcciones2[i] = { SaltosSubRed: saltosRed, nhosts: hosts, mascara: mascaraSubred };
+    }
+
+    let tablaDirecciones2 = document.getElementById("direcciones2");
+    let cuerpoTabla2 = document.createElement("tbody");
+
+    direcciones2.forEach(p => {
+        let fila = document.createElement("tr");
+
+        td = document.createElement("td");
+        td.innerText = p.SaltosSubRed;
+        fila.appendChild(td);
+
+        td = document.createElement("td");
+        td.innerText = p.nhosts;
+        fila.appendChild(td);
+
+        td = document.createElement("td");
+        td.innerText = p.mascara;
+        fila.appendChild(td);
+
+        cuerpoTabla2.appendChild(fila);
+    });
+    tablaDirecciones2.appendChild(cuerpoTabla2);
 }
